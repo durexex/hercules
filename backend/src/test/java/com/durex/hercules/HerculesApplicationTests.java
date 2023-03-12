49c22c95a7442273484ec6c137b5726ca6dc29ba
durexex <eduardodurieuxlopes@gmail.com>
2023-03-12 08:15:21 -0300

    Primeiro commit projeto

---
+++ backend/src/test/java/com/durex/hercules/HerculesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durex.hercules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerculesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
